5072acf1d9e9fde8fce0ca92eee6443d24f52f44
JParis44 <j.paris.caldwell@gmail.com>
2015-06-12 12:16:34 -0500

    completed graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -4,24 +4,57 @@ var Graph = function(){
 };
 
 Graph.prototype.addNode = function(node){
+  this[node] = {};
+  //debugger;
 };
 
 Graph.prototype.contains = function(node){
+  var keys = Object.keys(this);
+  //debugger;
+  for(var i = 0; i < keys.length; i++){
+    if(keys[i] === node){
+      return true;
+    }
+  }
+  return false;
 };
 
 Graph.prototype.removeNode = function(node){
+  if(this[node] !== {}){
+    var nodes = Object.keys(this[node]);
+    for(var i = 0; i < nodes.length; i++){
+      delete(this[nodes[i]].node);
+
+    }
+  }
+  delete this[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  if(this[fromNode].hasOwnProperty(toNode)){
+    return true;
+  }
+  return false;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this[fromNode][toNode] = null;
+  this[toNode][fromNode] = null;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  delete this[fromNode][toNode];
+  delete this[toNode][fromNode];
+
 };
 
 Graph.prototype.forEachNode = function(cb){
+  var nodes = Object.keys(this);
+  debugger;
+  for(var i = 0; i < nodes.length; i++){
+    cb(nodes[i]);
+  }
+
 };
 
 /*
